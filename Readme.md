fe928fa3e135093d1270db72290fb60588123c5d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to draw a summary cell under the indicator column
+
+
+<p>This example demonstrates how to draw a summary cell within an empty space under the row indicator column</p>
+
+<br/>
+
+
---
+++ VB/Form1.vb
@@ -0,0 +1,125 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports System.Windows.Forms
+Imports System.Data
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
+
+Namespace SumUnderIndicator
+	''' <summary>
+	''' Summary description for Form1.
+	''' </summary>
+	Public Class Form1
+		Inherits System.Windows.Forms.Form
+		Private gridControl1 As DevExpress.XtraGrid.GridControl
+		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.Container = Nothing
+
+		Public Sub New()
+			'
+			' Required for Windows Form Designer support
+			'
+			InitializeComponent()
+
+			'
+			' TODO: Add any constructor code after InitializeComponent call
+			'
+		End Sub
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		Protected Overrides Overloads Sub Dispose(ByVal disposing As Boolean)
+			If disposing Then
+				If components IsNot Nothing Then
+					components.Dispose()
+				End If
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' gridControl1
+			' 
+			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			' 
+			' gridControl1.EmbeddedNavigator
+			' 
+			Me.gridControl1.EmbeddedNavigator.Name = ""
+			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+			Me.gridControl1.MainView = Me.gridView1
+			Me.gridControl1.Name = "gridControl1"
+			Me.gridControl1.Size = New System.Drawing.Size(656, 410)
+			Me.gridControl1.TabIndex = 0
+			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+			' 
+			' gridView1
+			' 
+			Me.gridView1.GridControl = Me.gridControl1
+			Me.gridView1.Name = "gridView1"
+'			Me.gridView1.CustomDrawFooter += New DevExpress.XtraGrid.Views.Base.RowObjectCustomDrawEventHandler(Me.gridView1_CustomDrawFooter);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+			Me.ClientSize = New System.Drawing.Size(656, 410)
+			Me.Controls.Add(Me.gridControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+		#End Region
+
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread> _
+		Shared Sub Main()
+			Application.Run(New Form1())
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+			Dim TempXViewsPrinting As DevExpress.XtraGrid.Design.XViewsPrinting = New DevExpress.XtraGrid.Design.XViewsPrinting(gridControl1)
+			gridView1.OptionsView.ShowFooter = True
+			gridView1.IndicatorWidth = 50
+
+		End Sub
+
+		Private Sub gridView1_CustomDrawFooter(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.RowObjectCustomDrawEventArgs) Handles gridView1.CustomDrawFooter
+			e.Painter.DrawObject(e.Info)
+			Dim view As GridView = TryCast(sender, GridView)
+			Dim gvi As GridViewInfo = TryCast(view.GetViewInfo(), GridViewInfo)
+
+			Dim info As New DevExpress.Utils.Drawing.FooterCellInfoArgs(e.Cache)
+			info.Appearance.Assign(view.PaintAppearance.FooterPanel)
+			Dim rect As New Rectangle(e.Bounds.X, e.Bounds.Y, view.IndicatorWidth, e.Bounds.Height)
+			rect.Inflate(-3, -3)
+			info.Bounds = rect
+			info.DisplayText = String.Format("{0}", view.DataRowCount)
+			gvi.Painter.ElementsPainter.FooterCell.DrawObject(info)
+
+			e.Handled = True
+		End Sub
+	End Class
+End Namespace
